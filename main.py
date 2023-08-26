d7bd5eebdfafe52c494ba96c0c5066461e58ae6b
losper <novakakub@gmail.com>
2023-08-26 17:39:23 +0300

    обновление с дипПавловым

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_distances
+from deeppavlov import build_model
 
 df = pd.read_pickle("emb_sn-xlm-roberta-base-snli-mnli-anli-xnli.pkl")
 
@@ -15,7 +16,7 @@ def find_similar_questions(question):
     distances = cosine_distances(question_embedding, embeddings)
     
     # Поиск индексов топ-5 наименее удаленных вопросов
-    top_indices = distances.argsort()[0][:5]
+    top_indices = distances.argsort()[0][:2]
     
     # Получение топ-5 наиболее похожих вопросов и соответствующих ответов
     similar_questions = df.loc[top_indices, ['QUESTION', 'ANSWER']]
@@ -30,3 +31,22 @@ similar_questions = find_similar_questions(user_question)
 
 # Вывод результатов в формате датафрейма
 print(similar_questions)
+
+def extract_answers(similar_questions):
+    answers_list = similar_questions['ANSWER'].tolist()
+    answers_string = ' '.join(answers_list)
+    return [answers_string]
+
+answers_list = extract_answers(similar_questions)
+
+print(answers_list, type(answers_list))
+
+
+print(answers_list)
+print(str(user_question))
+# Использование модели для ответа на вопросы
+model_1 = build_model('squad_ru_bert', download=True)
+results = model_1(answers_list, user_question)
+
+print(results)
+
