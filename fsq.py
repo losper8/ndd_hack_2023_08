a90fd3b78192c6070974c62a5a68ce2b45cba836
Egor <sidorov.egor.main@gmail.com>
2023-08-26 21:50:20 +0300

    telegram bot

---
+++ fsq.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_distances
+
+df = pd.read_pickle("emb_sn-xlm-roberta-base-snli-mnli-anli-xnli.pkl")
+
+model_name = "symanto/sn-xlm-roberta-base-snli-mnli-anli-xnli"
+model = SentenceTransformer(model_name)  # вызов класса
+embeddings = df["эмбеддинги"].tolist()
+
+
+def find_similar_questions(question):
+    # Вычисление эмбеддинга введенного вопроса
+    question_embedding = model.encode([question])
+
+    # Вычисление сходства между введенным вопросом и всеми вопросами в столбце эмбеддингов
+    distances = cosine_distances(question_embedding, embeddings)
+
+    # Поиск индексов топ-5 наименее удаленных вопросов
+    top_indices = distances.argsort()[0][:5]
+
+    # Получение топ-5 наиболее похожих вопросов и соответствующих ответов
+    similar_questions = df.loc[top_indices, ['QUESTION', 'ANSWER']]
+
+    return similar_questions
